4623a38d1e7e9b33054dcdd71212fdea97f1ae9d
intanju <sukmanintan161@gmail.com>
2026-01-10 08:56:44 +0700

    week11-deadlock-detection

--- praktikum/week11-deadlock-detection/code/deadlock_detection.py
+++ praktikum/week11-deadlock-detection/code/deadlock_detection.py
@@ -10,33 +10,37 @@ file_path = os.path.join(os.path.dirname(__file__), "dataset_deadlock.csv")
 with open(file_path, "r") as file:
     reader = csv.reader(file)
     next(reader)
-
     for row in reader:
         process.append(row[0])
         allocation.append(row[1])
         request.append(row[2])
 
-bisa_jalan = []
+finish = [False] * len(process)
+progress = True
+
+while progress:
+    progress = False
+    for i in range(len(process)):
+        if not finish[i]:
+            # jika resource yang diminta tidak dipegang proses lain
+            if request[i] not in allocation:
+                finish[i] = True
+                allocation[i] = "-"   # resource dilepas
+                progress = True
+
+print("== Deadlock Detection ==")
+print()
+
+deadlock_process = []
 
 for i in range(len(process)):
-    if request[i] not in allocation:
-        bisa_jalan.append(process[i])
+    if not finish[i]:
+        deadlock_process.append(process[i])
 
-print("==Deadlock Detection==")
-print()
-print("Jumlah proses :", len(process))
-print()
-print("Status sistem :")
-
-if len(bisa_jalan) == 0:
+if deadlock_process:
     print("Deadlock terdeteksi")
-    print()
-    print("Proses yang terlibat deadlock :")
-    for p in process:
+    print("Proses yang terlibat deadlock:")
+    for p in deadlock_process:
         print(p)
 else:
     print("Tidak terjadi deadlock")
-    print()
-    print("Proses yang dapat berjalan :")
-    for p in bisa_jalan:
-        print(p)
